2b1596d3924f3d7ab047dd55fed3210d9f97f4d3
Mansi Saxena <120730390+MansiSaxena12@users.noreply.github.com>
2025-06-29 16:35:57 +0530

    Delete Level1 directory

--- Level1/intomiles.java
+++
@@ -1,7 +0,0 @@
-public class intomiles {
-    public static void main(String[] args) {
-    int distance=10.8;
-    int miles=1.6*distance;
-    System.out.printf(" The distance  %.d km in miles is %.d",distance,miles);
-    }
-}
